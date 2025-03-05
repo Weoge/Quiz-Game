a09de92856ba55fa677c4f44fd29e7c8cbcc5a2d
Weoge <164536152+Weoge@users.noreply.github.com>
2025-03-05 19:34:56 +0300

    Update constants.py

--- Мир профессий/app/constants/main/constants.py
+++ Мир профессий/app/constants/main/constants.py
@@ -1,9 +1,18 @@
 import pygame
+import os
+import sys
 pygame.init()
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 GAME_NAME = "Мир профессий"
 SCREEN_SIZE = (640, 480)
-FONT = pygame.font.Font("app/assets/font/Milk(RUS BY LYAJKA) Regular.ttf", 24)
-SMALL_FONT = pygame.font.Font("app/assets/font/Milk(RUS BY LYAJKA) Regular.ttf", 16)
-SMALLER_FONT = pygame.font.Font("app/assets/font/Milk(RUS BY LYAJKA) Regular.ttf", 10)
+FONT = pygame.font.Font(resource_path("app/assets/font/Milk(RUS BY LYAJKA) Regular.ttf"), 24)
+SMALL_FONT = pygame.font.Font(resource_path("app/assets/font/Milk(RUS BY LYAJKA) Regular.ttf"), 16)
+SMALLER_FONT = pygame.font.Font(resource_path("app/assets/font/Milk(RUS BY LYAJKA) Regular.ttf"), 10)
 GITHUB_LINK = "https://github.com/Weoge/Quiz-Game"
