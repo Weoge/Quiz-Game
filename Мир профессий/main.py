3b6cfb319d92bf9393a994a743ba4b3e91332194
Weoge <164536152+Weoge@users.noreply.github.com>
2025-03-05 19:34:28 +0300

    Update main.py

--- Мир профессий/main.py
+++ Мир профессий/main.py
@@ -7,42 +7,52 @@ import app.functions.playButtonClicked as playButtonClicked
 import app.generator.genrerate_questions as genrerate_questions
 import json
 import random
+import os
+import sys
 
 questionNumber = 1
 correct_answers = 0
 gaming = False
 current_buttons = None
 
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
 def create_question_buttons(screen, question_data, profession, level):
+    """Create buttons for questions with answers"""
     answers = question_data["answers"]
     question_text = constants.FONT.render(question_data["question"], True, colors.WHITE)
     question_text_rect = question_text.get_rect(center=(constants.SCREEN_SIZE[0] // 2, 50))
     correct_answers_text = constants.FONT.render(f"Правильных ответов: {correct_answers}", True, colors.WHITE)
     correct_answers_text_rect = correct_answers_text.get_rect(center=(constants.SCREEN_SIZE[0] // 2, 10))
     button1 = Button(screen, 320, 100, 
-        pygame.image.load(f"app/assets/images/{random.choice(colors.BUTTON_COLORS)}_button.png"),
-        pygame.image.load(f"app/assets/images/{random.choice(colors.BUTTON_COLORS)}_button_hover.png"),
+        pygame.image.load(resource_path(f"app/assets/images/{random.choice(colors.BUTTON_COLORS)}_button.png")),
+        pygame.image.load(resource_path(f"app/assets/images/{random.choice(colors.BUTTON_COLORS)}_button_hover.png")),
         answers[0], 
         lambda: check_answer(profession, level, answers[0], screen),
         350, 50)
     button1.set_delay(0.6)
     button2 = Button(screen, 320, 200,
-        pygame.image.load(f"app/assets/images/{random.choice(colors.BUTTON_COLORS)}_button.png"),
-        pygame.image.load(f"app/assets/images/{random.choice(colors.BUTTON_COLORS)}_button_hover.png"),
+        pygame.image.load(resource_path(f"app/assets/images/{random.choice(colors.BUTTON_COLORS)}_button.png")),
+        pygame.image.load(resource_path(f"app/assets/images/{random.choice(colors.BUTTON_COLORS)}_button_hover.png")),
         answers[1],
         lambda: check_answer(profession, level, answers[1], screen),
         350, 50)
     button2.set_delay(0.8)
     button3 = Button(screen, 320, 300,
-        pygame.image.load(f"app/assets/images/{random.choice(colors.BUTTON_COLORS)}_button.png"),
-        pygame.image.load(f"app/assets/images/{random.choice(colors.BUTTON_COLORS)}_button_hover.png"),
+        pygame.image.load(resource_path(f"app/assets/images/{random.choice(colors.BUTTON_COLORS)}_button.png")),
+        pygame.image.load(resource_path(f"app/assets/images/{random.choice(colors.BUTTON_COLORS)}_button_hover.png")),
         answers[2],
         lambda: check_answer(profession, level, answers[2], screen),
         350, 50)
     button3.set_delay(1)
     button4 = Button(screen, 320, 400,
-        pygame.image.load(f"app/assets/images/{random.choice(colors.BUTTON_COLORS)}_button.png"),
-        pygame.image.load(f"app/assets/images/{random.choice(colors.BUTTON_COLORS)}_button_hover.png"),
+        pygame.image.load(resource_path(f"app/assets/images/{random.choice(colors.BUTTON_COLORS)}_button.png")),
+        pygame.image.load(resource_path(f"app/assets/images/{random.choice(colors.BUTTON_COLORS)}_button_hover.png")),
         answers[3],
         lambda: check_answer(profession, level, answers[3], screen),
         350, 50)
@@ -60,7 +70,7 @@ def check_answer(profession, level, button_text, screen):
     }
     question_file = file_mapping.get(profession)
     if question_file:
-        with open(f"app/assets/json/{question_file}", "r", encoding='utf-8') as f:
+        with open(resource_path(f"app/assets/json/{question_file}"), "r", encoding='utf-8') as f:
             questions = json.load(f)
             level_key = f"level_{level}"
             questions = questions[level_key]
@@ -87,7 +97,7 @@ def load_current_question(profession, level):
     }
     question_file = file_mapping.get(profession)
     if question_file:
-        with open(f"app/assets/json/{question_file}", "r", encoding='utf-8') as f:
+        with open(resource_path(f"app/assets/json/{question_file}"), "r", encoding='utf-8') as f:
             questions = json.load(f)
             level_questions = questions[f"level_{level}"]
             if f"question_{questionNumber}" in level_questions:
@@ -99,49 +109,49 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode(constants.SCREEN_SIZE)
     pygame.display.set_caption(constants.GAME_NAME)
-    pygame.display.set_icon(pygame.image.load("app/assets/images/icon.png"))
+    pygame.display.set_icon(pygame.image.load(resource_path("app/assets/images/icon.png")))
     screen.fill(colors.BLACK)
 
     play_Button = Button(screen, 320, 130,
-        pygame.image.load("app/assets/images/green_button.png"), 
-        pygame.image.load("app/assets/images/green_button_hover.png"), 
+        pygame.image.load(resource_path("app/assets/images/green_button.png")), 
+        pygame.image.load(resource_path("app/assets/images/green_button_hover.png")), 
         "Играть", 
         lambda: playButtonClicked.playButtonClicked(), 
         150, 50)
     github_button = githubButton.GithubButton(screen, 605, 445, 
-        pygame.image.load("app/assets/images/github_button.png"), 
-        pygame.image.load("app/assets/images/github_button_hover.png"))
+        pygame.image.load(resource_path("app/assets/images/github_button.png")), 
+        pygame.image.load(resource_path("app/assets/images/github_button_hover.png")))
     profession_engineer = Button(screen, 150, 200,
-        pygame.image.load("app/assets/images/green_button.png"), 
-        pygame.image.load("app/assets/images/green_button_hover.png"), 
+        pygame.image.load(resource_path("app/assets/images/green_button.png")), 
+        pygame.image.load(resource_path("app/assets/images/green_button_hover.png")), 
         "Инженер", 
         lambda: genrerate_questions.select_prof(1), 
         150, 50)
     profession_engineer.set_delay(0.1)
     profession_builder = Button(screen, 150, 270,
-        pygame.image.load("app/assets/images/orange_button.png"), 
-        pygame.image.load("app/assets/images/orange_button_hover.png"), 
+        pygame.image.load(resource_path("app/assets/images/orange_button.png")), 
+        pygame.image.load(resource_path("app/assets/images/orange_button_hover.png")), 
         "Строитель", 
         lambda: genrerate_questions.select_prof(2), 
         150, 50)
     profession_builder.set_delay(0.3)
     profession_banker = Button(screen, 150, 340,
-        pygame.image.load("app/assets/images/red_button.png"), 
-        pygame.image.load("app/assets/images/red_button_hover.png"), 
+        pygame.image.load(resource_path("app/assets/images/red_button.png")), 
+        pygame.image.load(resource_path("app/assets/images/red_button_hover.png")), 
         "Банкир", 
         lambda: genrerate_questions.select_prof(3), 
         150, 50)
     profession_banker.set_delay(0.5)
     level_1 = Button(screen, 320, 200,
-        pygame.image.load("app/assets/images/green_button.png"), 
-        pygame.image.load("app/assets/images/green_button_hover.png"), 
+        pygame.image.load(resource_path("app/assets/images/green_button.png")), 
+        pygame.image.load(resource_path("app/assets/images/green_button_hover.png")), 
         "Уровень 1", 
         lambda: genrerate_questions.select_level(1), 
         150, 50)
     level_1.set_delay(0.1)
     level_2 = Button(screen, 320, 270,
-        pygame.image.load("app/assets/images/yellow_button.png"), 
-        pygame.image.load("app/assets/images/yellow_button_hover.png"), 
+        pygame.image.load(resource_path("app/assets/images/yellow_button.png")), 
+        pygame.image.load(resource_path("app/assets/images/yellow_button_hover.png")), 
         "Уровень 2", 
         lambda: genrerate_questions.select_level(2), 
         150, 50)
